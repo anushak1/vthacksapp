0ecb2c9fb5961b3977025865dead0557193fcc46
Anusha Khan <anushak@Anushas-MacBook-Pro.local>
2017-02-18 16:25:06 -0500

    Messages sync to firebase

--- ChatChat/ChatViewController.swift
+++ ChatChat/ChatViewController.swift
@@ -25,15 +25,27 @@ import Firebase
 import JSQMessagesViewController
 
     final class ChatViewController: JSQMessagesViewController {
-        override func viewDidAppear(_ animated: Bool) {
-             super.viewDidAppear(animated)
-            // messages from someone else
-            addMessage(withId: "foo", name: "Mr.Bolt", text: "I am so fast!")
-            // messages sent from local sender
-            addMessage(withId: senderId, name: "Me", text: "I bet I can run faster than you!")
-            addMessage(withId: senderId, name: "Me", text: "I like to run!")
-            // animates the receiving of a new message on the view
-            finishReceivingMessage()
+        private func observeMessages() {
+            messageRef = channelRef!.child("messages")
+            // 1.
+            let messageQuery = messageRef.queryLimited(toLast:25)
+            
+            // 2. We can use the observe method to listen for new
+            // messages being written to the Firebase DB
+            newMessageRefHandle = messageQuery.observe(.childAdded, with: { (snapshot) -> Void in
+                // 3
+                let messageData = snapshot.value as! Dictionary<String, String>
+                
+                if let id = messageData["senderId"] as String!, let name = messageData["senderName"] as String!, let text = messageData["text"] as String!, text.characters.count > 0 {
+                    // 4
+                    self.addMessage(withId: id, name: name, text: text)
+                    
+                    // 5
+                    self.finishReceivingMessage()
+                } else {
+                    print("Error! Could not decode message data")
+                }
+            })
         }
     var messages = [JSQMessage]()
         lazy var outgoingBubbleImageView: JSQMessagesBubbleImage = self.setupOutgoingBubble()
@@ -58,17 +70,17 @@ import JSQMessagesViewController
                 return incomingBubbleImageView
             }
         }
-//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = super.collectionView(collectionView, cellForItemAt: indexPath) as! JSQMessagesCollectionViewCell
-//        let message = messages[indexPath.item]
-//        
-//        if message.senderId == senderId {
-//            cell.textView?.textColor = UIColor.white
-//        } else {
-//            cell.textView?.textColor = UIColor.black
-//        }
-//        return cell
-//    }
+ override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+let cell = super.collectionView(collectionView, cellForItemAt: indexPath) as! JSQMessagesCollectionViewCell
+let message = messages[indexPath.item]
+    
+ if message.senderId == senderId {
+ cell.textView?.textColor = UIColor.white
+} else {
+     cell.textView?.textColor = UIColor.black
+     }
+     return cell
+  }
         
         override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
             return nil
@@ -93,6 +105,7 @@ import JSQMessagesViewController
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    observeMessages()
     
     self.senderId = FIRAuth.auth()?.currentUser?.uid
     // No avatars
