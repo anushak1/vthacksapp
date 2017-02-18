766b76544afdd071214b1b5e160206b4e50dae0f
tocheng1 <tony@Tonys-MacBook-Pro-2.local>
2017-02-18 14:45:05 -0500

    File and variables made for new user creation view

---
+++ ChatChat/CreateUserViewController.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  ChatChat
+//
+//  Created by Tony on 2/18/17.
+//  Copyright © 2017 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class CreateUserViewController: UIViewController {
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var capitalOneUsername: UITextField!
+    @IBOutlet weak var capitalOnePassword: UITextField!
+
+
+}
